ca35dece3ed53cbe2fc420a1a95f9ea561c62a98
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2024-11-08 02:24:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return <div>
+    this my proj.
+    <div><div className="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+<div className="alert alert-secondary" role="alert">
+  A simple secondary alert—check it out!
+</div></div>
+  </div>
+}
+
+export default App
